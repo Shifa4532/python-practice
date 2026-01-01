6c1875aaa0e6efbc55692b4a22f834a2c2f33b4c
shifa4532 <shi567fa@gmail.com>
2026-01-01 12:50:23 +0530

    edit

---
+++ part1.py
@@ -0,0 +1,60 @@
+# a = (input("name :"))
+# b = (input("age :"))
+# print("Hello " + a +", You are " + b + " years old")
+
+
+# num_1 = int(input("number 1:"))
+# num_2 = int(input("number 2:"))
+# sum = num_1 + num_2
+# difference = num_1 - num_2
+# product = num_1 * num_2 
+# quotient = num_1 / num_2
+# print (sum , difference , product , quotient)
+
+
+# val_1 = float(input("enter a decimal number:"))
+# val_2 = float(input("enter an integer:"))
+# val_3 = float(input("enter an integer again:"))
+# average = (val_1 + val_2 + val_3) /3
+# print(average)
+
+
+# var_1 = str(input("enter a number:"))
+# a=int(var_1)
+# b=float(var_1)
+# c=str(var_1)
+# print(a, b, c)
+# print(type(a),type(b),type(c))
+
+
+# x=10+3*2**2
+# print(x)
+
+
+# x=input("enter a number: ")
+# y=input("enter another number: ")
+# x,y=y,x
+# print(x,y)
+
+
+# temp_cel = float(input("enter temperature in celsius: "))
+# calc_temp = (temp_cel * (9/5)) +32
+# print(calc_temp)
+
+
+# pi=3.14
+# radius=int(input("enter the radius: "))
+# area=pi*(radius)**2
+# print(area)
+
+
+# principal_val = float(input("enter the principal amount: "))
+# rate_val = float(input("enter the rate: "))
+# time_val = float(input("enter the time in months: "))
+# s_i = (principal_val * rate_val * (time_val / 12)) / 100
+# print(s_i)
+
+
+# inp_val=float(input("enter a decimal number: "))
+# print(int(inp_val))
+# print(float(float(inp_val)-int(inp_val)))
---
+++ part2.py
@@ -0,0 +1,90 @@
+# salary=int(input("enter your salary: "))
+# if salary < 30000:
+#    print("tax levied on you is 5%")
+# elif (salary >= 30000 and salary <= 70000):
+#    print("tax levied on you is 15%")
+# else:
+#    print("tax levied on you is 25%")
+
+
+# a=int(input("enter a number a: "))
+# b=int(input("enter another number b: "))
+# for n in range(a,b+1,1):
+#    if n%2 == 0 :
+#      print(n)
+
+
+# n=int(input("enter a number: "))
+# def give_digits(n):
+#     if n==0:
+#         return
+#     give_digits(n // 10)
+#     print(n % 10) 
+# give_digits(n)
+
+
+# n=int(input("number: "))
+# def get_count(n):
+#     if n == 0:
+#         return 1 
+#     count = 0
+#     while n > 0:
+#        n //= 10
+#        count += 1
+#     return count
+# print(get_count(n)) 
+
+
+# n=int(input("enter a no. to get a sum: "))
+# def sum_digits(n):
+#     if n == 0:
+#         return 0
+#     return (n % 10) + sum_digits(n // 10) 
+# print(sum_digits(n))
+
+
+# def div_by_3_n_5():
+#     for n in range(1,101):
+#       if n % 3 == 0 and n % 5 == 0 :
+#          print(n)
+# div_by_3_n_5()
+
+
+# while True :
+#   n=input("enter any number: ") 
+#   if n == "quit" :
+#     print("end")
+#     break
+#   n=int(n)
+#   if n > 0 :
+#     print("positive")
+#   elif n < 0 :
+#     print("negative")
+#   else:
+#     print("zero")
+
+
+# def calculator (a, b, operation):
+#     if operation == "+":
+#         return a+b
+#     elif operation == "-":
+#         return a-b
+#     elif operation == "*":
+#         return a*b
+#     elif operation == "/":
+#         return a/b
+#     else:
+#         print("error")
+# result=calculator (5, 4, "+")
+# print(result)
+
+
+n=int(input("check if num is prime: "))
+def is_prime (n):
+     if n < 2:
+        return False
+     for i in range (2, n):
+         if n % i == 0 :
+            return False
+     return True
+print(is_prime(n))
