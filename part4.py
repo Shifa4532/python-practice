10012dec0701ec808a32418c6d19be5c0f71bfe1
shifa4532 <shi567fa@gmail.com>
2026-01-10 19:30:39 +0530

    edit 4.1

--- part4.py
+++ part4.py
@@ -10,8 +10,12 @@ class product :
     @classmethod
     def get_count(cls):
         print(f"total products in store = {cls.count}")
+    @staticmethod
+    def calc_discount(price, discount) :
+        print(f"discounted price = {price - (price * discount/100)}")
 p1 = product("phone", 10_000)
 p2 = product("laptop", 40_000)
 p3 = product("pen", 10)
 p1.get_info()
 product.get_count()
+p1.calc_discount(p1.price, 12)
