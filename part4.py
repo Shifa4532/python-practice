28532915530221a55c8d0a668d044204e5b1ce04
shifa4532 <shi567fa@gmail.com>
2026-01-12 22:30:24 +0530

    edit 4.31

--- part4.py
+++ part4.py
@@ -20,23 +20,48 @@
 # p1.withdraw(5000)
 
 
-class book :
-    def __init__(self, title, author):
-       self.title = title
-       self.author = author 
-       self.reviews = []
-    def add_review (self, rev) :
-        self.reviews.append(rev)
-    def count_review(self) :
-        return len(self.reviews)
-    def display_reviews (self):
-        if not self.reviews :
-            print("no reviews yet!")
+# class book :
+#     def __init__(self, title, author):
+#        self.title = title
+#        self.author = author 
+#        self.reviews = []
+#     def add_review (self, rev) :
+#         self.reviews.append(rev)
+#     def count_review(self) :
+#         return len(self.reviews)
+#     def display_reviews (self):
+#         if not self.reviews :
+#             print("no reviews yet!")
+#         else :
+#             for i, review in enumerate(self.reviews, start = 1) :
+#                 print(f"{i}. {review}")
+# b1 = book("ikikgai", "garcia")
+# b1.add_review("spiritual :)")
+# b1.add_review("peaceful")
+# print("total reviews: ", b1.count_review())
+# b1.display_reviews() 
+
+
+class student:
+    def __init__(self, name, roll_no, marks) : 
+       self.__name = name
+       self.__roll_no = roll_no
+       self.__marks = marks
+    def get_name (self) :
+        return self.__name 
+    def set_name (self, name) :
+        if isinstance(name, str) and name.strip() != "" :
+          self.__name = name
         else :
-            for i, review in enumerate(self.reviews, start = 1) :
-                print(f"{i}. {review}")
-b1 = book("ikikgai", "garcia")
-b1.add_review("spiritual :)")
-b1.add_review("peaceful")
-print("total reviews: ", b1.count_review())
-b1.display_reviews() 
+            print("Invalid name! Name cannot be empty.")
+    def get_roll_no(self) :
+       return self.__roll_no
+    def set_roll_no (self, roll_no) :
+        if isinstance(roll_no, int) and 1 <= roll_no <= 100:
+          self.__roll_no = roll_no
+        else :
+           print("Invalid roll number! Must be between 1 and 100.")
+   #  def get_marks(self) :
+   #     return self.__marks
+   #  def set_marks(self) :
+       
