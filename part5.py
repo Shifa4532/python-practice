1707831c215f7f7199d146ee6eca4d7e30f8865e
shifa4532 <shi567fa@gmail.com>
2026-01-14 23:16:59 +0530

    edit 5.1

--- part5.py
+++ part5.py
@@ -1,1 +1,9 @@
-#lol
+with open("names.txt", "w") as f:
+    for i in range(5):
+        name = input("enter name: ")
+        f.write(name + "\n")
+with open("names.txt", "r") as f:
+    content = f.read()
+    print(content) 
+
+
