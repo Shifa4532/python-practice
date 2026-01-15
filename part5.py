8009599e7eb83bd665342b3e38cdd01b478dc3bb
shifa4532 <shi567fa@gmail.com>
2026-01-15 14:56:18 +0530

    edit 5.4

--- part5.py
+++ part5.py
@@ -18,20 +18,22 @@
 # print(new_list)
 
 
-import json
-data = {
-    "mumbai" : 100000 ,
-    "delhi" : 200000 ,
-    "lucknow" : 300000
-}
-with open("cities.json", "w") as f:
-    json.dump(data, f)
-with open("cities.json", "r") as f:
-    data = json.load(f)
-for city, population in data.items():
-    print(f"{city}:{population}")    
-new_city = input("enter city:")
-new_pop = input("enter population:")
-data[new_city] = new_pop
-with open("cities.json", "w") as f:
-    json.dump(data, f)
+# import json
+# data = {
+#     "mumbai" : 100000 ,
+#     "delhi" : 200000 ,
+#     "lucknow" : 300000
+# }
+# with open("cities.json", "w") as f:
+#     json.dump(data, f)
+# with open("cities.json", "r") as f:
+#     data = json.load(f)
+# for city, population in data.items():
+#     print(f"{city}:{population}")    
+# new_city = input("enter city:")
+# new_pop = input("enter population:")
+# data[new_city] = new_pop
+# with open("cities.json", "w") as f:
+#     json.dump(data, f)
+
+
