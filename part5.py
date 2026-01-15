ea98b213d325d4a72a2d9c9ee1afd3e9a33bc064
shifa4532 <shi567fa@gmail.com>
2026-01-15 14:44:20 +0530

    edit 5.4

--- part5.py
+++ part5.py
@@ -13,8 +13,25 @@
 #      print(f.read())
     
 
-numbers = [5, 10, 15, 20, 25]
-new_list = [ num for num in numbers if num > 15 ]
-print(new_list)
+# numbers = [5, 10, 15, 20, 25]
+# new_list = [ num for num in numbers if num > 15 ]
+# print(new_list)
 
 
+import json
+data = {
+    "mumbai" : 100000 ,
+    "delhi" : 200000 ,
+    "lucknow" : 300000
+}
+with open("cities.json", "w") as f:
+    json.dump(data, f)
+with open("cities.json", "r") as f:
+    data = json.load(f)
+for city, population in data.items():
+    print(f"{city}:{population}")    
+new_city = input("enter city:")
+new_pop = input("enter population:")
+data[new_city] = new_pop
+with open("cities.json", "w") as f:
+    json.dump(data, f)
