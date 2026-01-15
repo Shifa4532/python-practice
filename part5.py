6a85f8de39030be04c67aa114a6d73ed885537d3
shifa4532 <shi567fa@gmail.com>
2026-01-15 13:37:03 +0530

    edit 5.3

--- part5.py
+++ part5.py
@@ -7,9 +7,14 @@
 #     print(content) 
 
 
-with open("log.txt", "a") as f:
-    f.write("program chal gya!")
-with open("log.txt", "r") as f:
-     print(f.read())
+# with open("log.txt", "a") as f:
+#     f.write("program chal gya!")
+# with open("log.txt", "r") as f:
+#      print(f.read())
     
+
+numbers = [5, 10, 15, 20, 25]
+new_list = [ num for num in numbers if num > 15 ]
+print(new_list)
+
 
